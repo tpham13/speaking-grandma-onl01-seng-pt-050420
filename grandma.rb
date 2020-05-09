05dd190220fb734ed9f2d6007c050abe47a39710
Thanh Pham <tpham0123@gmail.com>
2020-05-09 18:57:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(phrase)
-  if phrase = "Hi grandma"
+  if phrase == "Hi grandma"
     puts "HUH?!SPEAK UP, SONNY!"
-  elsif phrase = "HI GRANDMA"
+  elsif phrase == "HI GRANDMA"
     puts "NO, NOT SINCE 1938!"
-  else phrase = "I LOVE YOU GRANDMA!"
+  else phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
 end
 
