d6c211507e466f282d17c7b7554d97bfe3a5e2a3
Thanh Pham <tpham0123@gmail.com>
2020-05-09 19:07:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(phrase)
   elsif phrase == phrase.upcase 
     return "NO, NOT SINCE 1938!"
   else phrase == "Hi grandma!"
-    return "HUH?! SPEAK UP SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
