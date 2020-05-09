dd909dc16b67cdc11e777a653e120bea91beabf5
Thanh Pham <tpham0123@gmail.com>
2020-05-09 18:58:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,6 +5,7 @@ def speak_to_grandma(phrase)
     return "NO, NOT SINCE 1938!"
   else phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  end
 end
 
 # Whatever you say to grandma, she should respond with
